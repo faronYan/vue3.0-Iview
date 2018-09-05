8816f607629493a52db1bd7fa0e77fb9132a0041
faron_y <faron_y@sina.com>
2018-09-05 14:59:18 +0800

    add echarts

--- src/main.js
+++ src/main.js
@@ -1,18 +1,21 @@
 import Vue from 'vue'
-import App from './App.vue'
 import router from './router'
 import store from './store/store'
 import iView from 'iview';
 import ElementUI from 'element-ui';
-import 'element-ui/lib/theme-chalk/index.css';
+// import 'element-ui/lib/theme-chalk/index.css';
 import 'iview/dist/styles/iview.css';
 import {mapGetters, mapActions, mapState, mapMutations} from 'vuex'
+import echarts from 'echarts';
+import App from './App.vue'
 import VueI18n from 'vue-i18n';
 import en from '@/Language/en';
 import zh from '@/Language/zh';
+
 Vue.use(VueI18n);
 Vue.use(iView);
 Vue.use(ElementUI);
+Vue.prototype.$echarts = echarts; /*注入e*/
 /*设置默认语言*/
 Vue.config.lang = store.state.userOperating.lang || 'en';
 const messages = {
--- src/views/Home/Home.vue
+++ src/views/Home/Home.vue
@@ -1,6 +1,7 @@
 <template>
   <div class="home">
     <h1>{{$t('home.Calculating')}}</h1>
+    <div id="myChart" :style="{width: '300px', height: '300px'}"></div>
   </div>
 </template>
 
@@ -12,6 +13,29 @@ export default {
   name: 'home',
   components: {
     HelloWorld
-  }
+  },
+    mounted(){
+      this.drawLine();
+    },
+    methods:{
+        drawLine(){
+            // 基于准备好的dom，初始化echarts实例
+            let myChart = this.$echarts.init(document.getElementById('myChart'))
+            // 绘制图表
+            myChart.setOption({
+                title: { text: '在Vue中使用echarts' },
+                tooltip: {},
+                xAxis: {
+                    data: ["衬衫","羊毛衫","雪纺衫","裤子","高跟鞋","袜子"]
+                },
+                yAxis: {},
+                series: [{
+                    name: '销量',
+                    type: 'pie',
+                    data: [5, 20, 36, 10, 10, 20]
+                }]
+            });
+        }
+    }
 }
 </script>
